839d26fde6c9d221f635fb4bb2ccf15fb3adcfc7
ashishsv99 <ashishsv99@gmail.com>
2023-11-10 15:17:34 +0530

    Add necessary files for pip install

---
+++ math_quiz/setup.py
@@ -0,0 +1,11 @@
+ 
+from setuptools import setup, find_packages
+
+setup(
+    name='math_quiz',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies your project needs
+    ],
+)
